24d22722f2d6dbfcf954264ca33e0ad4b407362f
kalyan <kalyanaram.geda@gmail.com>
2025-07-04 11:59:58 +0530

    Renamed login.html to index.html

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def get_db_connection():
 # Routes
 @app.route('/')
 def home():
-    return render_template('login.html')
+    return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
 def login():
